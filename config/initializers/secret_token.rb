6b9246ad77ae6386534372e83e1b953aa7c57a29
Shakil007 <mehedi_nsu07@hotmail.com>
2014-08-18 17:27:32 -0400

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kenakata::Application.config.secret_token = 'c0a9fec27af9dc075e2afbeb1d287b905c35d544a2085fb67d3a49fa2ee0a18aeebe76ac75c46b9aab5fe4a33b1b8db6bf0f4eb1c12c88835c44ec74d5e9f039'
